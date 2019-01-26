b3e87dbe82099c0de0249bf932f04e6ea9509c7f
Crews <danre1@bk.ru>
2019-01-26 17:36:46 +0300

    Initial commit

---
+++ resources/enemys.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/resources/main.atlas\"\n"
+  "default_animation: \"eneeeem\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_MULT\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 1.0
+    z: 0.0
+    w: 6.123234E-17
+  }
+}
